15c62c3ce726541ecd42df5b802388f798b5d46f
kris hays <krisjhays@gmail.com>
2018-06-15 16:50:09 -0400

    pushing up random number and suits generator

---
+++ blackjack_final.py
@@ -0,0 +1,19 @@
+import random
+
+suits = ['hearts', 'diamonds', 'spades', 'clovers']
+
+    
+suit_pick = random.choice(suits)
+
+        
+print (suit_pick)
+number_card = [1,2,3,4,5,6,7,8,9,10, "eleven", "twelve","thirteen"]
+
+    
+number_pick = random.choice(number_card)
+    
+    
+print(number_pick, suit_pick)
+    eleven = {jack:"10"}
+    twelve = {Queen:"10"}
+    thirteen = {king:"10"}
