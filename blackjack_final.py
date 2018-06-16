790e7c5d612d6e255e37f63d219f382326da9e21
kris hays <krisjhays@gmail.com>
2018-06-16 19:49:13 -0400

    made the choice for ace

--- blackjack_final.py
+++ blackjack_final.py
@@ -7,14 +7,18 @@ suit_pick = random.choice(suits)
 
         
 print (suit_pick)
-number_card = [2,3,4,5,6,7,8,9,10, "Jack", "Queen","King"]
-face_cards = {
-    Jack : 10,
-    Queen : 10,
-    King : 10
-}
+number_card = [2,3,4,5,6,7,8,9,10,"ace","Jack", "Queen","King"]
+
 number_pick = random.choice(number_card)
-if number_pick == "face_cards":
+face_cards = {
+    "Jack" : 10,
+    "Queen" : 10,
+    "King" : 10,
+}
+if number_pick == "ace":
+    ace_choice = input("do you want it to be a 1 or 11?")
+    
+    
     
     
 print(number_pick, suit_pick)
