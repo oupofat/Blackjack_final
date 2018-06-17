60dc10e94b779d566ae6c0ecc685361a1874f0e8
kris hays <krisjhays@gmail.com>
2018-06-16 20:24:20 -0400

    made the dictionary return the value for the face cards

--- blackjack_final.py
+++ blackjack_final.py
@@ -1,20 +1,20 @@
 import random
-
+#this chunk randoms a suit
 suits = ['hearts', 'diamonds', 'spades', 'clovers']
-
-    
-suit_pick = random.choice(suits)
-
-        
+suit_pick = random.choice(suits)      
 print (suit_pick)
-number_card = [2,3,4,5,6,7,8,9,10,"ace","Jack", "Queen","King"]
 
+#this chunk randoms the number and face cards.
+number_card = [2,3,4,5,6,7,8,9,10,"ace","Jack", "Queen","King"]
 number_pick = random.choice(number_card)
+
+
 face_cards = {
     "Jack" : 10,
     "Queen" : 10,
     "King" : 10,
 }
+return (face_cards[""])
 if number_pick == "ace":
     ace_choice = input("do you want it to be a 1 or 11?")
     
