72adecf42e0da7bec84fe70b8151948ede0f91de
kris hays <krisjhays@gmail.com>
2018-06-19 10:19:37 -0400

    pushing changes

--- practice.py
+++ practice.py
@@ -4,28 +4,27 @@ player1_card1 = 0
 player1_card2 = 0
 player2_card1 = 0
 player2_card2 = 0
+face_card = 0
+
 
 #this chunk randoms a suit
 suits = ['of Hearts', 'of Diamonds', 'of Spades', 'of Clovers']
 suit_pick = random.choice(suits)      
-print (suit_pick)
 
-def random_pick():
-
-    number_card = [2,3,4,5,6,7,8,9,10,"ace","Jack", "Queen","King"]
+def get_card_pick():
+    number_card = [2,3,4,5,6,7,8,9,10,"Ace","Jack", "Queen"]
     card_pick = random.choice(number_card)
-    print(card_pick)
     if card_pick == ("Jack"):
         face_card = 10
-    if card_pick == ("Queen"):
+    elif card_pick == ("Queen"):
         face_card = 10
-    if card_pick == ("King"):
+    elif card_pick == ("King"):
         face_card = 10
-    #if ace player chooses if they want a 1 or 11.
-    if card_pick == "ace":
+      #if ace player chooses if they want a 1 or 11.
+    elif card_pick == "Ace":
         face_card = int(input("do you want it to be a 1 or 11?"))
-    print(face_card)
-    return (card_pick or face_card)
-    
-random_pick()  
-print(random_pick, suit_pick)
+    else:
+        face_card = card_pick
+    return (face_card)
+player1_card1 = get_card_pick()
+print (player1_card1, suit_pick)
