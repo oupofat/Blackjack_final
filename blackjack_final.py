33d33065cafa44cd5535eeb9abde3e0a4e215fcc
kris hays <krisjhays@gmail.com>
2018-06-27 14:27:25 -0400

    fixed the counter function

--- blackjack_final.py
+++ blackjack_final.py
@@ -4,28 +4,40 @@ player1_card1 = 0
 player1_card2 = 0
 player2_card1 = 0
 player2_card2 = 0
+face_card = 0
+
 
 #this chunk randoms a suit
 suits = ['of Hearts', 'of Diamonds', 'of Spades', 'of Clovers']
 suit_pick = random.choice(suits)      
-print (suit_pick)
-
-def random_pick():
-
-    number_card = [2,3,4,5,6,7,8,9,10,"ace","Jack", "Queen","King"]
+#this grabs a random card number or face card
+def get_card_pick():
+    number_card = [2,3,4,5,6,7,9,10,"Ace","Jack","Queen","King"]
     card_pick = random.choice(number_card)
-    print(card_pick)
     if card_pick == ("Jack"):
         face_card = 10
-    if card_pick == ("Queen"):
+    elif card_pick == ("Queen"):
         face_card = 10
-    if card_pick == ("King"):
+    elif card_pick == ("King"):
         face_card = 10
-    #if ace player chooses if they want a 1 or 11.
-    if card_pick == "ace":
+      #if ace player chooses if they want a 1 or 11.
+    elif card_pick == "Ace":
         face_card = int(input("do you want it to be a 1 or 11?"))
-    print(face_card)
-    return (card_pick or face_card)
-    
-random_pick()  
-print(random_pick, suit_pick)
+    else:
+        face_card = card_pick
+    return (face_card)
+player1_card1 = get_card_pick()
+player1_card2 = get_card_pick()
+player2_card1 = get_card_pick()
+player2_card2 = get_card_pick()
+print (player1_card1, suit_pick)
+#this get the total for the players
+total_player1 = player1_card1 + player1_card2 
+#total_player2 = player2_card1 + player2_card2
+print (total_player1)
+#print (total_player2)
+if total_player1 >= 1:
+    print(total_player1)
+    hit_card = input("do you want a another card, y or n")
+    if hit_card == "y":
+        get_card_pick()
--- practice.py
+++ practice.py
@@ -2,17 +2,16 @@ import random
 #counter for players
 player1_card1 = 0
 player1_card2 = 0
-player2_card1 = 0
-player2_card2 = 0
+total_player1 = 0
 face_card = 0
 
 
 #this chunk randoms a suit
 suits = ['of Hearts', 'of Diamonds', 'of Spades', 'of Clovers']
 suit_pick = random.choice(suits)      
-
+#this grabs a random card number or face card
 def get_card_pick():
-    number_card = [2,3,4,5,6,7,8,9,10,"Ace","Jack", "Queen"]
+    number_card = [2,3,4,5,6,7,9,10,"Ace","Jack", "Queen"]
     card_pick = random.choice(number_card)
     if card_pick == ("Jack"):
         face_card = 10
@@ -20,11 +19,36 @@ def get_card_pick():
         face_card = 10
     elif card_pick == ("King"):
         face_card = 10
-      #if ace player chooses if they want a 1 or 11.
+    #if ace player chooses if they want a 1 or 11.
     elif card_pick == "Ace":
         face_card = int(input("do you want it to be a 1 or 11?"))
     else:
         face_card = card_pick
     return (face_card)
 player1_card1 = get_card_pick()
+player1_card2 = get_card_pick()
+#prints player ones cards
 print (player1_card1, suit_pick)
+print (player1_card2, suit_pick)
+#adds the total for the player one and prints it
+total_player1 = player1_card1 + player1_card2
+print (total_player1)
+#looking at trying to put the hit card into a function
+def card_total():
+    hit_card = 0
+    total = 0
+    while True:
+        if total <= 21:
+            hit_me = input("do you want a another card, hit or n")
+            if hit_me == "hit":
+                hit_card = get_card_pick()
+                hit_card += int(input("add card to your hand"))
+                total = hit_card 
+            if hit_card > 21:
+                print ("You lose")
+                return (total)
+            if hit_me == "n":
+                print("Stay")
+                print(total)
+                return (total)
+card_total()
