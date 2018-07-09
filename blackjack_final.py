273bc282f016e1a22fa9be6f8a543283ec8c58bc
kris hays <krisjhays@gmail.com>
2018-07-09 16:02:29 -0400

    Cleaned up the verbage, move the face cards to local card pull function

--- blackjack_final.py
+++ blackjack_final.py
@@ -1,28 +1,11 @@
 import random
-print ("**********************************")
-print ("*welcome to the game of Blackjack*")
-print ("*      closest to 21 wins        *")
-print ("* Don't go over or you will lose *")
-print ("**********************************")
-
-#import statements
-#counter for player
-player1_card1 = 0
-player1_card2 = 0
-total_player1 = 0
-face_card = 0
-
-#dealer draw cards
-dealer_card1 = 0 
-dealer_card2 = 0 
-total_dealer = 0 
-face_card = 0 
 
 #function definitions
 
 #this grabs a random card number or face card
-def get_card_pick():
+def deal_random_card():
       #this chunk randoms a suit
+    face_card = 0 
     suits = ['of Hearts','of Diamonds', 'of Spades', 'of Clovers']
     suit_pick = random.choice(suits)
 
@@ -36,19 +19,19 @@ def get_card_pick():
         face_card = 10
     #if ace player chooses if they want a 1 or 11.
     elif card_pick == "Ace":
-        face_card = int(input("do you want it to be a 1 or 11?"))
+        face_card = int(input("Do you want it to be a 1 or 11?"))
     else:
         face_card = card_pick
     return(face_card,suit_pick)
 
     #looking at trying to put the hit card into a function
-def card_total(total_player1):
+def get_player_cards(total_player1):
     hit_card = 0
     while True:
         if total_player1 <= 21:            
-            hit_me = input("do you want a another card, hit or n")
+            hit_me = input("Would you like a another card, hit or n")
             if hit_me == "hit":
-                hit_card = get_card_pick()
+                hit_card = deal_random_card()
                 print(hit_card)
                 total_player1 += hit_card[0]
                 print("Your new total is ", total_player1)
@@ -61,12 +44,12 @@ def card_total(total_player1):
                 return (total_player1)
 
 #dealer draw cards
-def dealer_cards (total_dealer):
+def draw_cards_for_dealer(total_dealer):
     draw_card = 0 
 
     while True:
         if total_dealer <= 16:
-            draw_card = get_card_pick()
+            draw_card = deal_random_card()
             print(draw_card)
             total_dealer += draw_card[0]
         if total_dealer >= 17:
@@ -78,21 +61,40 @@ def dealer_cards (total_dealer):
                 return(total_dealer)
 
 
+print ("**********************************")
+print ("*welcome to the game of Blackjack*")
+print ("*      closest to 21 wins        *")
+print ("* Don't go over or you will lose *")
+print ("**********************************")
+
+#import statements
+#counter for player
+player1_card1 = 0
+player1_card2 = 0
+total_player1 = 0
+
+#dealer draw cards
+dealer_card1 = 0 
+dealer_card2 = 0 
+total_dealer = 0 
+
+
+
 #variable initialization
 
 #prints player ones cards
-player1_card1 = get_card_pick()
+player1_card1 = deal_random_card()
 print (player1_card1)
-player1_card2 = get_card_pick()
+player1_card2 = deal_random_card()
 print (player1_card2)
 #adds the total for the player one and prints it grabbing the first postion in the tuple.
 total_player1 = (player1_card1[0] + player1_card2[0])
 print ("Your total is" , total_player1, "Player one")
 
 #dealer cards section
-dealer_card1 = get_card_pick()
+dealer_card1 = deal_random_card()
 print(dealer_card1)
-dealer_card2 = get_card_pick()
+dealer_card2 = deal_random_card()
 print(dealer_card2)
 #adds the total for the dealer prints it grabbing the first postions in the tuple
 total_dealer = (dealer_card1[0] + dealer_card2[0])
@@ -101,7 +103,7 @@ total_dealer = (dealer_card1[0] + dealer_card2[0])
 #main program
 
 #the actual game play 
-def game_play(dealer, player1):
+def run_game_play_find_winner(dealer, player1):
     if player1 < dealer:
         print("dealer wins")
     if player1 > dealer:
@@ -109,11 +111,11 @@ def game_play(dealer, player1):
     if player1 == dealer:
         print("its a tie! Push!!")
     
-dealer = dealer_cards(total_dealer)
+dealer = draw_cards_for_dealer(total_dealer)
 #print("dealer",dealer)
 if dealer <=21:
-  player1 = card_total(total_player1)  
+  player1 = get_player_cards(total_player1)  
   #print("player1",player1) 
   if player1 <= 21:
 
-    game_play(dealer,player1)  
+    run_game_play_find_winner(dealer,player1)  
